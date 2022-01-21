2210823a295006de317437eae7cb4213e55b1aa7
zhaoweijie <949210784@qq.com>
2022-01-21 16:43:39 +0800

    修改前端标题

--- vue-admin/src/locales/lang/en/sys.ts
+++ vue-admin/src/locales/lang/en/sys.ts
@@ -71,7 +71,7 @@ export default {
     signUpFormTitle: 'Sign up',
     forgetFormTitle: 'Reset password',
 
-    signInTitle: 'Backstage management system',
+    signInTitle: 'QueYouQuan Backstage management system',
     signInDesc: 'Enter your personal details and get started!',
     policy: 'I agree to the xxx Privacy Policy',
     scanSign: `scanning the code to complete the login`,
